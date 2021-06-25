d4a79bfe7c7bb677746f778f34908a08e323879f
Angel Dzhambazov <a.dzhambazov@seeburger.com>
2021-06-25 14:39:49 +0300

    This is working code for creating pet with given ID name and status

--- src/main/java/org/angel/rest/Constants.java
+++ src/main/java/org/angel/rest/Constants.java
@@ -6,9 +6,9 @@ public class Constants
 
 
 	public static final String petStoreURL = "https://petstore.swagger.io/";
-	public static final String suffixAccessPet = "v2/pet/";
-	public static final String suffixAccessStore = "v2/store/";
-	public static final String suffixAccessUser = "v2/user/";
+	public static final String suffixAccessPet = "v2/pet";
+	public static final String suffixAccessStore = "v2/store";
+	public static final String suffixAccessUser = "v2/user";
 
 
 	public static final String GET = "GET";
--- src/main/java/org/angel/rest/RestTest.java
+++ src/main/java/org/angel/rest/RestTest.java
@@ -3,8 +3,12 @@ package org.angel.rest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class RestTest
 {
@@ -14,10 +18,22 @@ public class RestTest
 	{
 		System.out.println("Hello Move Digital!");
 
-		// TODO replace with try catch later
-		int petID = 16;
-		basicURLtoExecute = Constants.petStoreURL + Constants.suffixAccessPet + petID;
+		// this is post method to create pet
+		postAnimalByIDandName(23, "Pet23", "sold");
+
+		// this is GET method to check if created pet is seen.
+		getPetByID(23);
+
+		System.out.println("end");
+
+	}
+
+	private static void getPetByID(int id) throws MalformedURLException, IOException, ProtocolException
+	{
+		int petID = id;
+		basicURLtoExecute = Constants.petStoreURL + Constants.suffixAccessPet + "/" + petID;
 		URL url = new URL(basicURLtoExecute);
+		System.out.println(url.toString());
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setRequestMethod("GET");
 
@@ -37,9 +53,36 @@ public class RestTest
 		in.close();
 
 		con.disconnect();
+	}
 
-		System.out.println("end");
+	private static void postAnimalByIDandName(int id, String name, String status)
+			throws MalformedURLException, IOException, ProtocolException
+	{
+		System.out.println("we will execute post command for pet:");
+		System.out.println("ID = " + id + " name = " + name + " status = " + status);
+		basicURLtoExecute = Constants.petStoreURL + Constants.suffixAccessPet;
+		URL url = new URL(basicURLtoExecute);
+		HttpURLConnection http = (HttpURLConnection) url.openConnection();
+		http.setRequestMethod("POST");
+		http.setDoOutput(true);
+		http.setRequestProperty("accept", "application/json");
+		http.setRequestProperty("Content-Type", "application/xml");
 
+		String data = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Pet>\n	<id>" + id
+				+ "</id>\n	<Category>\n		<id>" + id + "</id>\n		<name>" + name
+				+ "</name>\n	</Category>\n	<name>" + name + "</name>\n	<tags>\n		<Tag>\n			<id>" + id
+				+ "</id>\n			<name>" + name + "</name>\n		</Tag>\n	</tags>\n	<status>" + status
+				+ "</status>\n</Pet>";
 
+		byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+		OutputStream stream = http.getOutputStream();
+		stream.write(out);
+
+		System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+		http.disconnect();
+
+		System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+		http.disconnect();
 	}
 }
